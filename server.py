999ae14b3803d825f354df339646b0bb5370681e
DanielBachler <daniel.bachler@comcast.net>
2019-09-19 18:28:14 -0600

    Testing

--- server.py
+++ server.py
@@ -8,12 +8,13 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind socket for local client
 localSocket.bind(("127.0.0.1", 8080))
 # Bind socket for "enemy" client
-#clientSocket.bind(("192.168.0.122", 8081))
+print(socket.gethostbyname("Das-boot"))
+clientSocket.bind((socket.gethostbyname("Das-boot"), 8081))
 
 #Connect to local client
 localSocket.listen(1)
 #Connect to enemy client
-#clientSocket.listen(1)
+clientSocket.listen(1)
 
 
 while True:
@@ -22,6 +23,6 @@ while True:
     print(f"got connected to {addr}")
     conn.send(bytes("Hi this is the server", "utf-8 "))
     # Enemy client connection
-    #connEnemy, addrEnemy = clientSocket.accept()
-    #print(f"got connected to {addrEnemy}")
-    #connEnemy.send(bytes("Hi this is the server", "utf-8 "))
+    connEnemy, addrEnemy = clientSocket.accept()
+    print(f"got connected to {addrEnemy}")
+    connEnemy.send(bytes("Hi this is the enemy server", "utf-8 "))
