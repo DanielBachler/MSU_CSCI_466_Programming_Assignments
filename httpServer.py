debcf022980a8b8f0fd983d2b85fe36c851683c0
Dan <daniel.bachler@comcast.net>
2019-09-21 16:05:45 -0600

    Commented httpServer.py

--- httpServer.py
+++ httpServer.py
@@ -1,50 +1,65 @@
 #Dan and Logan, HTTP Server to run Battleship from
+# import needed libraries
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import socketserver
 import sys
-import numpy as np
 import re
 
-# Takes in the gameboard file and returns a 2D array of it
+# Takes in the gameboard file and returns a "2D array" of it
 def makeBoard(fileName):
+    # Open file provided by sys arg to read
     boardFile = open(fileName, 'r')
+    # Create an empty 10x10 matrix
     gameboard = [['0' for x in range(10)] for y in range(10)]
+    # Iterate over the 10 lines of the file
     for i in range(0,10):
+        # Get each line
         line = boardFile.readline()
-        #print(line)
+        # Iterate over the line and place each indivdual character into the matrix
         for j in range(0,10):
             gameboard[i][j] = line[j]
+    # Close the file to prevent memory leaks
     boardFile.close()
+    # Return completed gameboard object
     return gameboard
 
+# Prints the gameboard in a nicely formatted way
 def printBoard():
     for i in range(0,10):
         print(gameboard[i])
 
-# Takes the POST request and determines if that hits or not
+# Takes the POST request and determines how to reply
 def handlePost(request):
+    # Cleaning the input of unneeded characters
     request = re.sub('[yx=]', '', request)
     request = re.sub('[&]', ' ', request)
+    # Splits the request into 2 strings on the space added by regex
     request = request.split(" ")
     
+    # checking for request formatting errors, if not what is expected then return bad formatting
     try:
         x = int(request[0])
         y = int(request[1])
     except:
         return "bad-req"
-    #global gameboard
-    # C B R S D
-    # If out of bounds handle
+    # If coords are out of bounds sends that request
     if((x > 9 and x < 0) or (y > 9 or y < 0)):
         return "oob"
     # Get value into one var so I dont have to type gameboard[x][y] a thousand times
     target = gameboard[x][y]
+
     # Target handling
+    # If the square being shot is not water keep going through logic tree
+    # else return miss val
     if(target != '_'): 
+        # If the targetted square is a letter and lower case it has been shot already
         if(target.islower()):
             return "hit rep"
+        # Sets the hit square to lower to show it has been shot at
         gameboard[x][y] = target.lower()
+        # Check if that shot sank the ship
+        # If it didn't, then return normal hit
         if(didSink(target)):
             return "sank " + target
         else:
@@ -52,13 +67,16 @@ def handlePost(request):
     else:
         return "miss"
 
+# Checks if a given ship is sunk or not
 def didSink(shipType):
-    shipSunk = True
+    # Assume the ship is sunk until proven otherwise
+    # Iterate over the entire board, looking for an uppercase marker 
     for i in range(0,10):
         for j in range(0,10):
+            # If an uppercase marker is found return false
             if gameboard[i][j] == shipType:
-                shipSunk = False
-    return shipSunk
+                return False
+    return True
 
 # The base form for server and request handler taken from online examples
 # Found at: (TODO insert)
@@ -67,10 +85,10 @@ def didSink(shipType):
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     # When contacted by a client, send gameboard
     def do_GET(self):
-        # Needed
+        # Needed for "proper" request
         self.send_response(200)
         self.end_headers()
-        # Send message
+        # Prints gameboard to local site
         for i in range(0,10):
             for j in range(0,10):
                 self.wfile.write(bytes(gameboard[i][j], "UTF-8"))
@@ -79,12 +97,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     # Handling POST requests
     def do_POST(self):
         # Gets the length of the header as an integer
-        content_length = int(self.headers['Content-Length'])
+        # content_length = int(self.headers['Content-Length'])
         # Sets body equal to the value of the content in the packet
-        body = self.rfile.read(content_length)
+        # body = self.rfile.read(content_length)
+
         # Get the value of result from handlePost, and send the correct HTTP message
         result = handlePost(self.path)
-        # "miss", "hit", "sank D", "hit rep", "oob"
+
+        # Possible values returned
+        # "miss", "hit", "sank [CBRSD]", "hit rep", "oob", "bad-req"
         if(result == "miss"):
             self.send_response(200, "hit={}".format(0))
         elif(result == "hit"):
@@ -93,7 +114,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(410)
         elif(result == "oob"):
             self.send_response(404)
-        # C B R S D
         elif(result == "sank C"):
             self.send_response(200, "hit={}&sink={}".format(1,'C'))
         elif(result == "sank B"):
@@ -106,21 +126,26 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200, "hit={}&sink={}".format(1,'D'))
         elif(result == "bad-req"):
             self.send_response(400)
+        # Closes header, needed for proper formatted request
         self.end_headers()
+        # Print the board after each request
         printBoard()
         # Create BytesIO object
-        response = BytesIO()
+        # response = BytesIO()
         # Print out that this is a POST request 
-        response.write(b'This is POST request. ')
+        # response.write(b'This is POST request. ')
         # Writes out what was recieved
-        response.write(b'Received: ')
-        response.write(body)
+        # response.write(b'Received: ')
+        # response.write(body)
         # ?
-        self.wfile.write(response.getvalue())
+        #self.wfile.write(response.getvalue())
 
-# Init server on ip for localc computer on TCP port open to firewall
-httpd = HTTPServer(('100.64.11.151', int(sys.argv[1])), SimpleHTTPRequestHandler)
+# Init server on ip for given IP on TCP port provided by Sys ARGS.
+# Port must be open on firewall
+httpd = HTTPServer(('192.168.197', int(sys.argv[1])), SimpleHTTPRequestHandler)
+# Creates the board from the Sys ARG file
 gameboard = makeBoard(sys.argv[2])
+# Prints the inital board
 printBoard()
-# Server do server things forever
+# Server runs until ctrl+C is pressed in terminal
 httpd.serve_forever()
